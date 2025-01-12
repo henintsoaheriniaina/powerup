adceb122558e9be778f5d022efd805765eddc7a3
Henintsoa Heriniaina <belouhtsoa@gmail.com>
2025-01-12 11:54:04 +0300

    test

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,11 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
-
 export default {
-  light: {
-    text: '#000',
-    background: '#fff',
-    tint: tintColorLight,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorLight,
+  baseViolet: "#8E6CEF",
+  baseGray: {
+    light: "#F4F4F4",
+    dark: "#8E8E8E",
+    tabs: "#939393",
   },
-  dark: {
-    text: '#fff',
-    background: '#000',
-    tint: tintColorDark,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorDark,
-  },
+  baseBlack: "#272727",
+  baseRed: "#FA3636",
+  baseYellow: "#FFB632",
 };
